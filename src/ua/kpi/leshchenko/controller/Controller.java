b08a188acc18df1515a804887cfd8ddeb70ff2dd
vitaliaventel <vitaliaventel@gmail.com>
2016-11-23 19:58:23 +0200

    Application scope bets

--- src/ua/kpi/leshchenko/controller/Controller.java
+++ src/ua/kpi/leshchenko/controller/Controller.java
@@ -3,6 +3,7 @@ package ua.kpi.leshchenko.controller;
 import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +12,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.log4j.Logger;
 
 import ua.kpi.leshchenko.commands.ICommand;
+import ua.kpi.leshchenko.dao.DAOFactory;
+import ua.kpi.leshchenko.dao.EventDAO;
 
 /**
  * Servlet implementation class Controller
@@ -19,6 +22,7 @@ public class Controller extends HttpServlet {
 
 	private CHelper helper = CHelper.getInstance();
 	private static Logger logger = Logger.getLogger(Controller.class.getName());
+	private EventDAO daoEvent = DAOFactory.createEventDAO();
 
 	/**
 	 * @see HttpServlet#HttpServlet()
@@ -30,9 +34,12 @@ public class Controller extends HttpServlet {
 	protected void processRequest(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		String page = null;
+		ServletContext context = request.getSession().getServletContext();
 		try {
 			ICommand command = helper.getCommand(request);
 			page = command.execute(request, response);
+			context.setAttribute("eventListUp", daoEvent.findNotFinished());
+			context.setAttribute("eventListFinish", daoEvent.findFinished());
 		} catch (ServletException e) {;
 			logger.error("Servlet exception", e);
 		} catch (IOException e) {
